da97fa2a9766a733f2f04abc2f1ab39883eb5f26
Gilgames_ <22778436+Gilgames000@users.noreply.github.com>
2020-04-04 23:14:33 +0200

    Add EntityCondition packet

---
+++ packetsrv/cond_test.go
@@ -0,0 +1,33 @@
+package packetsrv
+
+import (
+	"testing"
+
+	"github.com/alecthomas/participle"
+	"github.com/gilgames000/go-noskit/parser"
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestEntityCondition(t *testing.T) {
+	packet := "cond 1 1879326 0 0 11"
+
+	expected := &EntityCondition{
+		EntityType:        1,
+		EntityID:          1879326,
+		IsAttackAllowed:   0,
+		IsMovementAllowed: 0,
+		Speed:             11,
+	}
+
+	out := &EntityCondition{}
+	err := participle.MustBuild(
+		out,
+		participle.Lexer(parser.NosLexer()),
+		participle.Elide("Whitespace"),
+	).ParseString(packet, out)
+
+	if !cmp.Equal(out, expected) || err != nil {
+
+		t.Errorf("\ncond packet parsing failed\npacket: %+v\nexpected: %+v\nparsed: %+v\nerror: %+v", packet, expected, out, err)
+	}
+}
