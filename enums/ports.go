11314caa6fc6222171fde0fa302cb8ecc175da69
Gilgames000 <22778436+Gilgames000@users.noreply.github.com>
2020-11-16 12:22:16 +0100

    Add ports

---
+++ enums/ports.go
@@ -0,0 +1,15 @@
+package enums
+
+type LoginPort string
+
+const (
+	LoginPortEN = "4000"
+	LoginPortDE = "4001"
+	LoginPortFR = "4002"
+	LoginPortIT = "4003"
+	LoginPortPL = "4004"
+	LoginPortES = "4005"
+	LoginPortCZ = "4006"
+	LoginPortRU = "4007"
+	LoginPortTR = "4008"
+)
